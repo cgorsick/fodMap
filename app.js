eb628127d6996c11ad3e239abfb7be44e5c43aa6
cynthia gorsick <cynthia gorsick>
2016-03-20 00:05:41 -0400

    Adding buttons in small view

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var store = angular.module('store', []);
 app.controller('storeController', ['$scope', function(){
-	$scope.productname = 'book',
-	$scope.product.price = '13.50',
-	$scope.product.description = "..."
+	product.name = 'book',
+	product.price = '13.50',
+	product.description = "..."
  }]);
